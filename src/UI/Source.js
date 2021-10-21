92534775b26647595c0f8e9c584019f6244be750
onlymachiavelli <tmakaveli643@gmail.com>
2021-10-21 20:05:59 +0100

    UI

--- src/UI/Source.js
+++ src/UI/Source.js
@@ -1,9 +1,12 @@
 import React from "react"
+import styles from "./../../styles/Home.module.css"
 
 const CurrencyContainer = ({ ...props }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <div></div>
     </div>
   )
 }
+
+export default CurrencyContainer
