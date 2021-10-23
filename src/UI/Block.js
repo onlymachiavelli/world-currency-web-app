7f029218c0cc15a390b6422f1c7a3d4785fc9636
Mahciavelli <tmakaveli643@gmail.com>
2021-10-23 22:10:10 +0100

    Created block file

---
+++ src/UI/Block.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './../../styles/Home.module.css'
+const Currency = ({...props})=>{
+    return(
+        <div className={styles.convertBlock}>
+            <div></div>
+        </div> 
+    )
+}
