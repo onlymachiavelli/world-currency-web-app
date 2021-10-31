e5868b18e9c75405b8a3d06a56b438aeac8446e5
onlymachiavelli <tmakaveli643@gmail.com>
2021-10-31 16:24:12 +0100

    Picking the current country code

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import Head from "next/head"
 import Navbar from "./../src/UI/navbar"
 
 import styles from "./../styles/Home.module.css"
 
 import CurrencyContainer from "../src/UI/Source"
+import callGeo from "./../src/apiCall/location"
+let imgApi = "https://flagcdn.com/w2560/za.png"
 
 const Home = () => {
+  let [currentCountryCode, setCurrentCountry] = useState("")
+  callGeo().then((res) => setCurrentCountry(res.countryCode))
+  console.log(currentCountryCode)
+
   return (
     <main>
       <Head>
