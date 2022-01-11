ee323744df016f99dacf225899a10fa1a891810b
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-11 23:17:17 +0100

    stuffs

--- src/apiCall/currencycall.js
+++ src/apiCall/currencycall.js
@@ -1,5 +1,5 @@
 import React from "react"
-import axios from 'axios'
-const callCurrency = async() => {}
+import axios from "axios"
+const callCurrency = async () => {}
 
 export default callCurrency
