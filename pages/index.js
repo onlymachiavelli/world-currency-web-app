1102868d7d14d163f835a60828cc8fc6ddd9055b
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-11 23:25:37 +0100

    api call currency

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react"
 import Head from "next/head"
 import Navbar from "./../src/UI/navbar"
-
+import axios from "axios"
 import styles from "./../styles/Home.module.css"
 
 import CurrencyContainer from "../src/UI/Source"
 import callGeo from "./../src/apiCall/location"
 let imgApi = "https://flagcdn.com/w2560/za.png"
+import callCurrency from "../src/apiCall/currencycall"
 
 const Home = () => {
-  let [currentCountryCode, setCurrentCountry] = useState("")
+  const [currentCountryCode, setCurrentCountry] = useState("")
+  const [test, setTest] = useState({})
   callGeo().then((res) => setCurrentCountry(res.countryCode))
-  console.log(currentCountryCode)
+  callCurrency().then((res) => {
+    setTest(res)
+  })
+
+  console.log(test)
 
   return (
     <main>
--- src/apiCall/currencycall.js
+++ src/apiCall/currencycall.js
@@ -1,5 +1,15 @@
 import React from "react"
 import axios from "axios"
-const callCurrency = async () => {}
+
+const callCurrency = async () => {
+  const APIKEY = "6a0d42ec238114b8ecce5cff1c7aa96a"
+
+  const respone = await axios
+    .get(`http://api.currencylayer.com/live?access_key=${APIKEY}`)
+    .then((res) => {
+      res.data
+    })
+  return respone
+}
 
 export default callCurrency
