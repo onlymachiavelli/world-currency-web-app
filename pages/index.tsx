d9cb42a5031776cab39ef7d7d363533e3e94a20a
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-13 19:34:19 +0100

    from scratch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,10 @@
-import React, { useState, useEffect } from "react"
-import Head from "next/head"
-import Navbar from "../src/UI/navbar"
-import axios from "axios"
-import styles from "./../styles/Home.module.css"
+import React, {} from 'react' 
 
-import CurrencyContainer from "../src/UI/Source"
-import callGeo from "../src/apiCall/location"
-let imgApi = "https://flagcdn.com/w2560/za.png"
-import callCurrency from "../src/apiCall/currencycall"
-
-const Home = () => {
-  const [currentCountryCode, setCurrentCountry] = useState("")
-  const [test, setTest] = useState({})
-
-  useEffect(()=>{
-    callGeo().then((res:any) => setCurrentCountry(res.countryCode))
-    callCurrency().then((res:any) => {
-      
-      console.log(res)
-    })
-  
-  }, [])
-
+const Home = () =>{
   return (
-    <main>
-      <Head>
-        <title>Convert Currencies</title>
-      </Head>
-
-      <main className={styles.body}>
-        <Navbar />
-        <CurrencyContainer Cflag="tn" Tflag="mn" />
-      </main>
-    </main>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
