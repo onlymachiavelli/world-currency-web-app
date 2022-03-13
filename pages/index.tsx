10558f12b247e1bca27102d514c403ba1e2c9bcb
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-13 19:51:35 +0100

    handling err

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
-import React, {} from 'react' 
+import React, {useState, useEffect} from 'react' 
+import callCurrency from '../src/apiCall/currencycall'
 
 const Home = () =>{
+  const [datas, setDatas] = useState({})
+  const [from, setFrom] = useState(1)
+  const [to, setTo] = useState(0)
+
+  useEffect(()=>{
+      callCurrency().then((response)=>{
+        setDatas(response)
+      })
+  }, [])
+  console.log(datas)
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <input type={"text"}  value={from} onChange={(e)=>{e.target.value}} />
+      <br/><br/>  
+      <input type={"text"} value={to}  onChange={(e)=>{e.target.value}} />
+
+      <button type="button" onClick={()=>{
+      }}>Transfer</button> 
+    </div>
   )
 }
 
