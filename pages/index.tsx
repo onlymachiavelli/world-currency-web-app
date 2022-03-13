6bce8fe08c8aa330956dc190f0aebdf5e8530220
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-13 23:30:23 +0100

    navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react' 
 import Navbar from '../src/UI/Navbar'
 import callCurrency from '../src/apiCall/currencycall'
-
+import Head from 'next/head'
 const Home = () =>{
   
 
   return (
     <main className='w-full h-screen'>
+      <Head>
+        <title>Currency Converter</title>
+      </Head>
       <Navbar/>
     </main>
   )
--- src/UI/Navbar.tsx
+++ src/UI/Navbar.tsx
@@ -1,20 +1,35 @@
 import React, {} from 'react'
 import Link from 'next/link'
-import {Logo} from './SVG'
+import {Logo, Instagram, Twitter, Linkedin, Github} from './SVG'
 
 const Navbar = () =>{
     return (
-        <nav className='flex w-full p-2 '>
+        <nav className='flex w-full p-2 items-center'>
             <div className=''>
                 <Link href="/">
                     <Logo Width="170" Height="30" />
                 </Link>
             </div> 
-            <nav className=''>
-                <Link href="/">Home</Link>
-                <Link href="">API</Link>
-                <Link href="">About</Link>
+            <nav className='flex gap-3 ml-7 mt-1.5' >
+                <Link href="/">
+                    <a className='link'>Home</a>
+                </Link>
+                <Link href="/">
+                    <a className='link'>API</a>
+                </Link>
+                <Link href="/">
+                    <a className='link'>Contact</a>
+                </Link>
+                <Link href="/">
+                    <a className='link'>About</a>
+                </Link>
             </nav>
+            <div className='flex absolute right-4 gap-2 mt-2'>
+                <a href=""><Github Width="15" Height="15" /></a>
+                <a href=""><Twitter Width="15" Height="15" /></a>
+                <a href=""><Instagram Width="15" Height="15" /></a>
+                <a href=""><Linkedin Width="15" Height="15" /></a>
+            </div>
         </nav>
     ) 
 
