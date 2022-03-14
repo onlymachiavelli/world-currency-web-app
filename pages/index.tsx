d4ca5bd570ff8532f39fc00d48da74c47750f442
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-14 18:34:47 +0100

    detect geo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import React, {useState, useEffect} from 'react' 
 import Navbar from '../src/UI/Navbar'
 import callCurrency from '../src/apiCall/currencycall'
+import callGeo from '../src/apiCall/location'
 import Head from 'next/head'
 import  CurrBlock  from './../src/UI/currencyBlock'
 const Home = () =>{
-  
-
+  const [currentCountry, setcurCountry] = useState("")
+  useEffect(()=>{
+    callGeo().then(
+      (res) =>{
+        console.log(res)
+      }
+    )
+  },[])
   return (
     <main className='w-full h-screen'>
       <Head>
@@ -13,7 +20,8 @@ const Home = () =>{
       </Head>
       <Navbar/>
       <p className='text-center text-white font-bold mt-4'>Converting World Wide Currencies and CryptoCurrencies</p>
-      <div className='w-4/5 h-6 bg-blue2 m-auto mt-7 rounded-lg p-10 flex items-center justify-center'>
+      <div className='w-4/5 h-auto gap-3 bg-blue2 m-auto mt-7 rounded-lg p-10 flex items-center justify-center'>
+        <CurrBlock enabled={true} />
         <CurrBlock enabled={true} />
       </div>
     </main>
--- src/UI/currencyBlock.tsx
+++ src/UI/currencyBlock.tsx
@@ -3,10 +3,16 @@ import {MoneyBag} from './SVG'
 const CurrBlock = ({...props}) => {
     
     return (
-        <div className=''>
-            <div>
-                <MoneyBag Width="20" Height="23" />
-                <input type={"text"} className='outline-0 p-1 text-center text-blue font-bold'/>
+        <div className=' justify-center items-center bg-blueSky w-2/5 h-auto p-5 rounded-lg pb-7'>
+            <div className='flex'>
+                <img src={props.Flag} />
+                <p className='text-white font-bold'>
+                    Hello
+                </p>
+            </div>
+            <div className='flex justify-center items-center bg-white p-2 '>
+                <MoneyBag Width="17" Height="19" />
+                <input type={"text"} className='outline-0 text-center text-blue font-bold bg-transparent'/>
             </div>
         </div>
     )
