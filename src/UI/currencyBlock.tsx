5a8712646dbb1bdc981475b526cf61cc67e892fc
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-14 21:02:12 +0100

    flag size

--- src/UI/currencyBlock.tsx
+++ src/UI/currencyBlock.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react"
-import {MoneyBag} from './SVG'
+import {MoneyBag, Down} from './SVG'
 const CurrBlock = ({...props}) => {
     
     return (
         <div className=' justify-center items-center bg-blueSky w-2/5 h-auto p-5 rounded-lg pb-7'>
             <div className='flex items-center'>
-                <img src={props.Flag} width={"60"} height="auto" className='rounded-ms' />
+                <div className=' block w-11 h-9 rounded-ms '>
+                <img src={props.Flag}  className='w-11 h-8'  />
+                </div>
                 <p className='text-white font-bold ml-2'>
                     {props.CountryName}
                 </p>
@@ -14,6 +16,9 @@ const CurrBlock = ({...props}) => {
                 <MoneyBag Width="17" Height="19" />
                 <input type={"text"} className='outline-0 text-center text-blue w-4/5 font-bold bg-transparent'/>
             </div>
+            <button className='block m-auto mt-3'>
+                <Down Width="25" Height="25" />
+            </button>
         </div>
     )
 }
