74aa33143d2e7e45601c8a051edfda2e616d9566
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-14 18:11:28 +0100

    some blocks

--- src/UI/currencyBlock.tsx
+++ src/UI/currencyBlock.tsx
@@ -1,7 +1,15 @@
-import React, {} from "react"
-
-const Block = ({...props}) => {
-    return <></>
+import React, { useState } from "react"
+import {MoneyBag} from './SVG'
+const CurrBlock = ({...props}) => {
+    
+    return (
+        <div className=''>
+            <div>
+                <MoneyBag Width="20" Height="23" />
+                <input type={"text"} className='outline-0 p-1 text-center text-blue font-bold'/>
+            </div>
+        </div>
+    )
 }
 
-export default Block
+export default CurrBlock
