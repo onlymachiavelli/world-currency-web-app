af0d4ad60969ca64a56a5dabbc37c38a11facf63
Mahciavelli <tmakaveli643@gmail.com>
2022-03-21 23:42:41 +0100

    fixing bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,58 @@
-import React, {useState, useEffect} from 'react' 
-import Navbar from '../src/UI/Navbar'
-import callCurrency from '../src/apiCall/currencycall'
-import callGeo from '../src/apiCall/location'
-import Head from 'next/head'
-import  CurrBlock  from './../src/UI/currencyBlock'
-import useConverter from '../src/hook/useConverter'
-import currencyMenu from '../src/UI/currencyMeny'
-import worldDatas from '../src/apiCall/worldWide'
+import React, { useState, useEffect } from "react"
+import Navbar from "../src/UI/Navbar"
+import callCurrency from "../src/apiCall/currencycall"
+import callGeo from "../src/apiCall/location"
+import Head from "next/head"
+import CurrBlock from "./../src/UI/currencyBlock"
+import useConverter from "../src/hook/useConverter"
+import currencyMenu from "../src/UI/currencyMeny"
+import worldDatas from "../src/apiCall/worldWide"
 
-import { countryCodes } from '../src/apiCall/co'
-const Home = () =>{
-  
-  const [from , setcFrom] = useState() 
+import { countryCodes } from "../src/apiCall/co"
+const Home = () => {
+  const [from, setcFrom] = useState()
   const [to, setTTo] = useState("us")
   const [inPval, setVal] = useState(1)
   const [geo, setGeo] = useState()
 
-  useEffect(()=>{
+  useEffect(() => {}, [])
 
-  },[])
-
-  const {response, setResponse, setFrom, setTcurrency, fromCurrency, toCurrency} = useConverter("us")
-    return (
-    <main className='w-full h-screen '>
+  const {
+    response,
+    setResponse,
+    setFrom,
+    setTcurrency,
+    fromCurrency,
+    toCurrency,
+  } = useConverter("us")
+  return (
+    <main className="w-full h-screen ">
       <Head>
         <title>Currency Converter</title>
       </Head>
-      <Navbar/>
-      
-      <div className='w-full h-max '>
-        <p className='text-center text-sm md:text-xl text-white font-bold mt-4'>Converting World Wide Currencies and CryptoCurrencies</p>
-        <div className='w-4/5 h-auto gap-3 bg-blue2 m-auto mt-7 rounded-lg p-14 flex items-center justify-center md:flex-row flex-col'>
-          <CurrBlock Flag={`https://flagcdn.com/h60/${fromCurrency["code"]}.png`} enabled={true} CountryName={fromCurrency.name} inputValue={inPval} />
-          <CurrBlock enabled={true} Flag={`https://flagcdn.com/w80/${toCurrency["code"]}.png`} CountryName={toCurrency.name} inputValue={response} />
+      <Navbar />
+
+      <div className="w-full h-max ">
+        <p className="text-center text-sm md:text-xl text-white font-bold mt-4">
+          Converting World Wide Currencies and CryptoCurrencies
+        </p>
+        <div className="w-4/5 h-auto gap-3 bg-blue2 m-auto mt-7 rounded-lg p-14 flex items-center justify-center md:flex-row flex-col">
+          <CurrBlock
+            Flag={`https://flagcdn.com/h60/${fromCurrency["code"]}.png`}
+            enabled={true}
+            CountryName={fromCurrency.name}
+            inputValue={inPval}
+          />
+          <CurrBlock
+            enabled={true}
+            Flag={`https://flagcdn.com/w80/${toCurrency["code"]}.png`}
+            CountryName={toCurrency.name}
+            inputValue={response}
+          />
         </div>
-        <button className='block m-auto mt-5 text-white font-bold bg-darkGreen md:w-56 w-3/4 h-12 rounded-sm hover:bg-green duration-1000'>Convert</button>
+        <button className="block m-auto mt-5 text-white font-bold bg-darkGreen md:w-56 w-3/4 h-12 rounded-sm hover:bg-green duration-1000">
+          Convert
+        </button>
       </div>
     </main>
   )
--- src/hook/useConverter.tsx
+++ src/hook/useConverter.tsx
@@ -1,51 +1,52 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react"
 
-import axios from 'axios'
-import callCurrency from '../apiCall/currencycall'
-import callGeo from '../apiCall/location'
-import worldDatas from '../apiCall/worldWide'
-const useConverter = (toIso:any):any =>{
-    const [fromIso, setFIso] = useState()
-    
-    const [fromCurrency, setFrom] = useState(
-        {
-            code:String,
-            name:String,
-            currency:String
-        }
-    )
-    const [toCurrency, setTcurrency] = useState({
-            code:String,
-            name:String,
-            currency:String
+import axios from "axios"
+import callCurrency from "../apiCall/currencycall"
+import callGeo from "../apiCall/location"
+import worldDatas from "../apiCall/worldWide"
+const useConverter = (toIso: any): any => {
+  const [fromIso, setFIso] = useState()
+
+  const [fromCurrency, setFrom] = useState({
+    code: String,
+    name: String,
+    currency: String,
+  })
+  const [toCurrency, setTcurrency] = useState({
+    code: String,
+    name: String,
+    currency: String,
+  })
+
+  const [response, setResponse] = useState(0)
+  useEffect((): void => {
+    callGeo().then((res) => {
+      setFIso(res["countryCode"])
     })
-    
-    const [response, setResponse] = useState(0) 
-    useEffect(():void=>{
-        callGeo().then(res=>{
-            setFIso(res["countryCode"])
-        })
-        console.log(fromIso)
-        worldDatas("iso2", fromIso, false).then(
-            res =>{
-                setFrom({
-                        code:res["data"]["iso2"].toLowerCase(),
-                        name:res["data"]["countryName"],
-                        currency:res["data"]["currencyCode"]
-                    })
-            }
-        )
-        worldDatas("iso2", toIso, false).then(
-            res =>{
-                setTcurrency({
-                    code:res["data"]["iso2"].toLowerCase(),
-                    name:res["data"]["countryName"],
-                    currency:res["data"]["currencyCode"],
-                })
-            }
-        )
-    }, [response,fromCurrency, toCurrency ,fromIso])
-    return {response, setResponse, setFrom, setTcurrency, fromCurrency, toCurrency}
+    console.log(fromIso)
+    worldDatas("iso2", fromIso, false).then((res) => {
+      setFrom({
+        code: res["data"]["iso2"].toLowerCase(),
+        name: res["data"]["countryName"],
+        currency: res["data"]["currencyCode"],
+      })
+    })
+    worldDatas("iso2", toIso, false).then((res) => {
+      setTcurrency({
+        code: res["data"]["iso2"].toLowerCase(),
+        name: res["data"]["countryName"],
+        currency: res["data"]["currencyCode"],
+      })
+    })
+  }, [])
+  return {
+    response,
+    setResponse,
+    setFrom,
+    setTcurrency,
+    fromCurrency,
+    toCurrency,
+  }
 }
 
 export default useConverter
